29c8153b68abbee3f116d09bc25bf469ba5c895c
PadLex <alexpadula2001@gmail.com>
2023-07-05 12:39:22 +0200

    test inline completion

--- out/extension.js
+++ out/extension.js
@@ -1,19 +1,99 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deactivate = exports.activate = exports.LudiiCompletionItemProvider = void 0;
+exports.deactivate = exports.activate = exports.LudiiCompletionItemProvider = exports.LudiiPredictionProvider = void 0;
 const vscode = require("vscode");
 const child_process_1 = require("child_process");
-const events_1 = require("events");
+//import { client } from "@gradio/client";
+class testClient {
+    async predict(endpoint, args) {
+        return new Promise((resolve, reject) => {
+            resolve([
+                {
+                    "value": "(game \"Hex\" (players 2) (equipment { (board (hex Diamond 11)) (piece \"Marker\" Each) (regions P1 {(sites Side NE) (sites Side SW)}) (regions P2 {(sites Side NW) (sites Side SE)})}) (rules (meta (swap)) (play (move Add (to (sites Empty)))) (end (if (is Connected Mover) (result Mover Win)))))"
+                }
+            ]);
+        });
+    }
+}
+class LudiiPredictionProvider {
+    constructor() {
+        this.model = "earthshine-nondecorative-2023-06-27-17-40-07";
+        this.template = "alpaca";
+        //client("https://7d55bed0b056f62d56.gradio.live/").then(gradioClient => this.gradioClient = gradioClient);
+        this.gradioClient = new testClient();
+    }
+    provideInlineCompletionItems(document, position, context, token) {
+        return new Promise((resolve, reject) => {
+            const game = getGame(document.getText());
+            console.log("PROVIDING INLINE COMPLETION ITEMS", game);
+            if (game.length == 0) {
+                const comments = getComments(document.getText());
+                console.log("COMMENTS: ", comments);
+                this.infer("Construct a Ludii game based on the following description", comments).then((prediction) => {
+                    console.log("PREDICTION: ", prediction);
+                    const completion = new vscode.InlineCompletionItem(prediction);
+                    resolve([completion]);
+                }).catch((error) => {
+                    console.log(error);
+                    reject([]);
+                });
+            }
+        });
+    }
+    async infer(instruction, input) {
+        const result = await this.gradioClient.predict("/inference", [
+            this.model,
+            this.template,
+            instruction,
+            input,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            0,
+            0.75,
+            40,
+            2,
+            1.2,
+            10,
+            false,
+            false // bool representing boolean value in 'Show Raw' Checkbox component
+        ]);
+        return result[0]['value'];
+    }
+    dispose() {
+        // TODO
+    }
+}
+exports.LudiiPredictionProvider = LudiiPredictionProvider;
+function getGame(text) {
+    text = text.replace(/(^|\n)\s*\/\/.*?($|\n)/g, '\n'); // Remove comments
+    text = text.replace(/\n/g, ' '); // Remove newlines
+    text = text.trim();
+    return text;
+}
+function getComments(text) {
+    let commentLines = text.match(/(^|\n)\s*\/\/.*?($|\n)/g);
+    let commentContents = "";
+    if (commentLines) {
+        commentContents += commentLines.map(line => line.replace(/(^|\n)\s*\/\/\s?/, '').trim()) + ' ';
+    }
+    return commentContents;
+}
 class LudiiCompletionItemProvider {
     constructor() {
         this.keepAlive = true;
-        [this.javaProcess, this.responseEmitter] = this.spawnJavaProcess();
+        this.javaProcess = this.spawnJavaProcess('approaches.symbolic.api.Autocomplete');
     }
     provideCompletionItems(document, position, token, context) {
         return new Promise((resolve, reject) => {
             let completionItems = [];
-            console.log('\nPROVIDING:', document.getText().replace('\n', ' '));
-            this.javaProcess.stdin.write(document.getText().replace('\n', ' ') + '\n');
+            let docText = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+            const text = getGame(docText);
+            console.log('\nPROVIDING:', text);
+            this.javaProcess.stdin.write(text + '\n');
             let result = '';
             const dataHandler = (data) => {
                 console.log('PARTIAL: ', data.at(data.length - 1));
@@ -45,8 +125,7 @@ class LudiiCompletionItemProvider {
             this.javaProcess.stderr.on('error', errorHandler);
         });
     }
-    spawnJavaProcess() {
-        const javaClass = 'approaches.symbolic.generators.DescriptionParser';
+    spawnJavaProcess(javaClass) {
         const projectRoot = '/Users/alex/Documents/Marble/Ludii/';
         const javaPath = [
             'Generation/bin',
@@ -58,21 +137,14 @@ class LudiiCompletionItemProvider {
             'Language/bin'
         ].map(module => projectRoot + module).join(':');
         const javaProcess = (0, child_process_1.spawn)('java', ['-cp', javaPath, javaClass]);
-        const responseEmitter = new events_1.EventEmitter();
-        // javaProcess.stdout.on('data', (data) => {
-        //     responseEmitter.emit('data', data);
-        // });
-        // javaProcess.stderr.on('data', (data) => {
-        //     responseEmitter.emit('error', data);
-        // });
         javaProcess.on('exit', () => {
             if (this.keepAlive) {
                 console.log('Java process exited. Restarting...');
-                [this.javaProcess, this.responseEmitter] = this.spawnJavaProcess();
+                this.javaProcess = this.spawnJavaProcess(javaClass);
             }
         });
         javaProcess.stdout.once('data', data => console.log('Startup:', data.toString()));
-        return [javaProcess, responseEmitter];
+        return javaProcess;
     }
     dispose() {
         this.keepAlive = false;
@@ -81,9 +153,11 @@ class LudiiCompletionItemProvider {
 }
 exports.LudiiCompletionItemProvider = LudiiCompletionItemProvider;
 const ludiiCompletionItemProvider = new LudiiCompletionItemProvider();
+const ludiiPredictionProvider = new LudiiPredictionProvider();
 function activate(context) {
     console.log('Ludii started');
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'ludii', scheme: 'file' }, ludiiCompletionItemProvider, ' ', '\t', '\n', '\r', '(', ')', '{', '}', ':', 'h'));
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'ludii', scheme: 'file' }, ludiiCompletionItemProvider));
+    context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider({ language: 'ludii', scheme: 'file' }, ludiiPredictionProvider));
 }
 exports.activate = activate;
 function deactivate() {
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,111 @@
 import * as vscode from 'vscode';
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
-import { EventEmitter } from 'events';
+//import { client } from "@gradio/client";
+
+class testClient {
+    public async predict(endpoint: string, args: any[]): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve([
+                {
+                    "value": "(game \"Hex\" (players 2) (equipment { (board (hex Diamond 11)) (piece \"Marker\" Each) (regions P1 {(sites Side NE) (sites Side SW)}) (regions P2 {(sites Side NW) (sites Side SE)})}) (rules (meta (swap)) (play (move Add (to (sites Empty)))) (end (if (is Connected Mover) (result Mover Win)))))"
+                }
+            ]);
+        });
+    }
+}
+
+
+export class LudiiPredictionProvider implements vscode.InlineCompletionItemProvider {
+    private gradioClient: any;
+    private model = "earthshine-nondecorative-2023-06-27-17-40-07";
+    private template = "alpaca";
+
+    constructor() {
+        //client("https://7d55bed0b056f62d56.gradio.live/").then(gradioClient => this.gradioClient = gradioClient);
+        this.gradioClient = new testClient();
+    }
+
+    public provideInlineCompletionItems(
+        document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext, token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.InlineCompletionItem[]> {
+        return new Promise((resolve, reject) => {
+            const game = getGame(document.getText());
+            console.log("PROVIDING INLINE COMPLETION ITEMS", game);
+
+            if (game.length == 0) {
+                const comments = getComments(document.getText());
+                console.log("COMMENTS: ", comments);
+                this.infer("Construct a Ludii game based on the following description", comments).then((prediction: string) => {
+                    console.log("PREDICTION: ", prediction);
+                    const completion = new vscode.InlineCompletionItem(prediction);
+                    resolve([completion]);
+                }).catch((error: any) => {
+                    console.log(error);
+                    reject([]);
+                });
+            }
+            
+        });
+    }
+
+    public async infer(instruction: string, input: string): Promise<string> {
+        const result = await this.gradioClient.predict(
+            "/inference", 
+            [
+                this.model,	// str representing Option from: [] in 'LoRA Model' Dropdown component
+                this.template,	// str representing Option from: [] in 'Prompt Template' Dropdown component
+                instruction,	// str representing string value in 'Prompt' Textbox component
+                input,	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                "",	// str representing string value in '' Textbox component
+                0,	// int | float representing numeric value between 0 and 2 in 'Temperature' Slider component
+                0.75,	// int | float representing numeric value between 0 and 1 in 'Top P' Slider component
+                40,	// int | float representing numeric value between 0 and 100 in 'Top K' Slider component
+                2,	// int | float representing numeric value between 1 and 5 in 'Beams' Slider component
+                1.2,	// int | float representing numeric value between 0 and 2.5 in 'Repetition Penalty' Slider component
+                10,	// int | float representing numeric value between 0 and 4096 in 'Max New Tokens' Slider component
+                false,	// bool representing boolean value in 'Stream Output' Checkbox component
+                false	// bool representing boolean value in 'Show Raw' Checkbox component
+            ]
+        );
+
+        return result[0]['value'];
+    }
+
+    public dispose() {
+        // TODO
+    }
+}
+
+function getGame(text: string): string {
+    text = text.replace(/(^|\n)\s*\/\/.*?($|\n)/g, '\n'); // Remove comments
+    text = text.replace(/\n/g, ' '); // Remove newlines
+    text = text.trim();
+    return text;
+}
+
+function getComments(text: string): string {
+    let commentLines = text.match(/(^|\n)\s*\/\/.*?($|\n)/g);
+
+    let commentContents = "";
+    if (commentLines) {
+        commentContents += commentLines.map(line => line.replace(/(^|\n)\s*\/\/\s?/, '').trim()) + ' ';
+    }
+    return commentContents;
+}
 
 
 export class LudiiCompletionItemProvider implements vscode.CompletionItemProvider {
     private javaProcess: ChildProcessWithoutNullStreams;
-    private responseEmitter: EventEmitter;
     private keepAlive: boolean = true;
 
 
     constructor() {
-        [this.javaProcess, this.responseEmitter] = this.spawnJavaProcess();
+        this.javaProcess = this.spawnJavaProcess('approaches.symbolic.api.Autocomplete');
     }
 
     public provideCompletionItems(
@@ -19,9 +114,13 @@ export class LudiiCompletionItemProvider implements vscode.CompletionItemProvide
 
         return new Promise((resolve, reject) => {
             let completionItems: vscode.CompletionItem[] = [];
-            
-            console.log('\nPROVIDING:', document.getText().replace('\n', ' '));
-            this.javaProcess.stdin.write(document.getText().replace('\n', ' ') + '\n');
+
+            let docText = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+
+            const text = getGame(docText);
+            console.log('\nPROVIDING:', text);
+
+            this.javaProcess.stdin.write(text + '\n');
             
             let result = '';
             const dataHandler = (data: Buffer) => {
@@ -59,8 +158,7 @@ export class LudiiCompletionItemProvider implements vscode.CompletionItemProvide
         });
     }
 
-    public spawnJavaProcess(): [ChildProcessWithoutNullStreams, EventEmitter] {
-        const javaClass = 'approaches.symbolic.generators.DescriptionParser';
+    public spawnJavaProcess(javaClass: string): ChildProcessWithoutNullStreams {
         const projectRoot = '/Users/alex/Documents/Marble/Ludii/';
         const javaPath = [
             'Generation/bin',
@@ -73,26 +171,17 @@ export class LudiiCompletionItemProvider implements vscode.CompletionItemProvide
         ].map(module => projectRoot + module).join(':');
     
         const javaProcess = spawn('java', ['-cp', javaPath, javaClass]);
-        const responseEmitter = new EventEmitter();
-
-        // javaProcess.stdout.on('data', (data) => {
-        //     responseEmitter.emit('data', data);
-        // });
-
-        // javaProcess.stderr.on('data', (data) => {
-        //     responseEmitter.emit('error', data);
-        // });
 
         javaProcess.on('exit', () => {
             if (this.keepAlive) {
                 console.log('Java process exited. Restarting...');
-                [this.javaProcess, this.responseEmitter] = this.spawnJavaProcess();
+                this.javaProcess = this.spawnJavaProcess(javaClass);
             }
         });
 
         javaProcess.stdout.once('data', data => console.log('Startup:', data.toString()));
 
-        return [javaProcess, responseEmitter];
+        return javaProcess;
     }
 
     public dispose() {
@@ -103,6 +192,7 @@ export class LudiiCompletionItemProvider implements vscode.CompletionItemProvide
 
 
 const ludiiCompletionItemProvider = new LudiiCompletionItemProvider();
+const ludiiPredictionProvider = new LudiiPredictionProvider();
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Ludii started');
@@ -110,8 +200,12 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
         { language: 'ludii', scheme: 'file' },
-        ludiiCompletionItemProvider,
-        ' ', '\t', '\n', '\r', '(', ')', '{', '}', ':', 'h'
+        ludiiCompletionItemProvider
+    ));
+
+    context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
+        { language: 'ludii', scheme: 'file' },
+        ludiiPredictionProvider
     ));
 }
 
