30df548c6c1be5f0b79610a3d988c1d845612997
PadLex <alexpadula2001@gmail.com>
2023-07-09 11:42:43 -0700

    Add templete ui

--- out/extension.js
+++ out/extension.js
@@ -3,17 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const javaController_1 = require("./javaController");
-const autocomplete_1 = require("./autocomplete");
-const inlinePredict_1 = require("./inlinePredict");
-const ludiiCompletionItemProvider = new autocomplete_1.LudiiAutocomplete();
-const ludiiPredictionProvider = new inlinePredict_1.LudiiPredictionProvider();
+const ui_1 = require("./ui");
+// const ludiiCompletionItemProvider = new LudiiAutocomplete();
+// const ludiiPredictionProvider = new LudiiPredictionProvider();
 function activate(context) {
     console.log('Ludii started');
     // context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
     //     { language: 'ludii', scheme: 'file' },
     //     ludiiCompletionItemProvider
     // ));
-    context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider({ language: 'ludii', scheme: 'file' }, ludiiPredictionProvider));
+    // context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
+    //     { language: 'ludii', scheme: 'file' },
+    //     ludiiPredictionProvider
+    // ));
+    const provider = new ui_1.ColorsViewProvider(context.extensionUri);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(ui_1.ColorsViewProvider.viewType, provider));
+    context.subscriptions.push(vscode.commands.registerCommand('ludii.addColor', () => {
+        provider.addColor();
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('ludii.clearColors', () => {
+        provider.clearColors();
+    }));
 }
 exports.activate = activate;
 function deactivate() {
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,10 @@ import * as vscode from 'vscode';
 import { disposeControllers } from './javaController';
 import { LudiiAutocomplete } from './autocomplete';
 import { LudiiPredictionProvider } from './inlinePredict';
+import { ColorsViewProvider }from './ui';
 
-const ludiiCompletionItemProvider = new LudiiAutocomplete();
-const ludiiPredictionProvider = new LudiiPredictionProvider();
+// const ludiiCompletionItemProvider = new LudiiAutocomplete();
+// const ludiiPredictionProvider = new LudiiPredictionProvider();
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Ludii started');
@@ -14,10 +15,28 @@ export function activate(context: vscode.ExtensionContext) {
     //     ludiiCompletionItemProvider
     // ));
 
-    context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
-        { language: 'ludii', scheme: 'file' },
-        ludiiPredictionProvider
-    ));
+    // context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
+    //     { language: 'ludii', scheme: 'file' },
+    //     ludiiPredictionProvider
+    // ));
+
+
+    const provider = new ColorsViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('ludii.addColor', () => {
+			provider.addColor();
+		}));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('ludii.clearColors', () => {
+			provider.clearColors();
+		}));
+
+
 }
 
 export function deactivate() {
